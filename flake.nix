1a6518e7e3dc49ed384b8d7305e17fdb3dffb446
will <will@256k1.dev>
2026-05-14 14:52:45 +0100

    WIP: use latest cmake on x64

--- flake.nix
+++ flake.nix
@@ -18,11 +18,38 @@
           lib = pkgs.lib;
           isLinux = pkgs.stdenv.hostPlatform.isLinux;
           llvm = pkgs.llvmPackages_latest;
+          cmake = if system == "x86_64-linux" then
+            pkgs.stdenvNoCC.mkDerivation rec {
+              pname = "kitware-cmake-bin";
+              version = "4.3.2";
+
+              src = pkgs.fetchurl {
+                url = "https://github.com/Kitware/CMake/releases/download/v${version}/cmake-${version}-linux-x86_64.tar.gz";
+                hash = "sha256-eRrjYEhBygPLOImjrYkWU0bksYCuNEjv1LDKqe9G0kU=";
+              };
+
+              sourceRoot = "cmake-${version}-linux-x86_64";
+              nativeBuildInputs = [ pkgs.autoPatchelfHook ];
+              buildInputs = [ pkgs.glibc ];
+
+              installPhase = ''
+                runHook preInstall
+
+                mkdir -p "$out"
+                cp -R . "$out"
+                rm -f "$out/bin/cmake-gui"
+                rm -rf "$out/doc"
+
+                runHook postInstall
+              '';
+            }
+          else
+            pkgs.cmakeCurses;
           commonNativeBuildInputs = [
             pkgs.bison
             pkgs.ccache
             pkgs.clang-tools
-            pkgs.cmakeCurses
+            cmake
             pkgs.curlMinimal
             pkgs.ninja
             pkgs.pkg-config
